4951d37e80757bb1b07392829a019ac64a29ef93
s95388 <146932591+s95388@users.noreply.github.com>
2023-11-29 11:44:27 +0100

    init lab08

---
+++ lab08-cli/src/main/java/pwo/lab08/cli/Lab08Cli.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pwo.lab08.cli;
+
+import pwo.lab08.engine.Engine;
+/**
+ *
+ * @author STUDENT
+ */
+public class Lab08Cli {
+
+    public static final String
+ APP_NAME = "Lab08 CLI Application ";
+
+ public static void main(String[] args) {
+ System.out.println(APP_NAME + Engine.getVersion() );
+ }
+}
---
+++ lab08-engine/src/main/java/pwo/lab08/engine/Engine.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pwo.lab08.engine;
+
+/**
+ *
+ * @author kacpe
+ */
+public class Engine {
+
+    public static final String
+ VERSION = "Number sequences engine version 1.0.0";
+
+ public static String getVersion() {
+ return VERSION;
+ }
+
+}
